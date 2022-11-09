9eb7219277fb39e1dc92dd47e74ed7298ebb698a
pseudoc <atlas.yu@canonical.com>
2022-11-10 00:46:16 +0800

    format code

--- apps/pwa-1024/vite.config.js
+++ apps/pwa-1024/vite.config.js
@@ -1,56 +1,52 @@
-import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
-import path from 'node:path';
+import path from "node:path";
 
 /**
  * @param {import('vite').ConfigEnv} env
  */
 function genius_config(env) {
   const generateScopedName =
-    env.mode === 'production' ? '[hash:base64:3]' : '[local]_[hash:base64:3]';
+    env.mode === "production" ? "[hash:base64:3]" : "[local]_[hash:base64:3]";
   /** @type {import('vite-plugin-pwa').VitePWAOptions} */
   const pwa_options = {
     mode: env.mode,
-    includeAssets: [
-      'favicon.svg'
-    ],
+    includeAssets: ["favicon.svg"],
     manifest: {
-      name: '1024 Game',
-      short_name: '1024',
-      theme_color: '#e76f51',
-      background_color: '#83c5be',
-      display: 'standalone',
+      name: "1024 Game",
+      short_name: "1024",
+      theme_color: "#e76f51",
+      background_color: "#83c5be",
+      display: "standalone",
       icons: [
         {
-          src: 'pwa-192x192.png',
-          sizes: '192x192',
-          type: 'image/png',
+          src: "pwa-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
         },
         {
-          src: 'pwa-512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
-          purpose: 'any maskable',
+          src: "pwa-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "any maskable",
         },
       ],
     },
     devOptions: {
-      enabled: env.mode !== 'production'
-    }
+      enabled: env.mode !== "production",
+    },
   };
   return defineConfig({
-    plugins: [
-      VitePWA(pwa_options)
-    ],
+    plugins: [VitePWA(pwa_options)],
     resolve: {
       alias: {
-        '@styles': path.resolve(__dirname, 'styles'),
+        "@styles": path.resolve(__dirname, "styles"),
       },
     },
     css: {
       modules: {
-        localsConvention: 'dashesOnly',
+        localsConvention: "dashesOnly",
         generateScopedName,
       },
     },
